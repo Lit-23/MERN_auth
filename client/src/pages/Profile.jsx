9eb02fc88928a53fbe3f3b95192734d5a131ae29
Lit-23 <rommelmarquez143@gmail.com>
2023-12-18 00:57:38 -0800

    added signout user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -8,7 +8,8 @@ import {
   updateUserFailure, 
   deleteUserStart, 
   deleteUserSuccess,
-  deleteUserFailure,  
+  deleteUserFailure,
+  signOut,  
 } from "../redux/user/userSlice.js";
 
 export default function Profile() {
@@ -89,8 +90,16 @@ export default function Profile() {
       dispatch(deleteUserFailure(error));
     }
   };
+  
+  const handleSignOut = async () => {
+    try {
+      await fetch('/api/auth/signout');
+      dispatch(signOut());
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  console.log(formData);
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -150,7 +159,7 @@ export default function Profile() {
       </form>
       <div className="flex justify-between mt-5">
         <span onClick={handleDeleteAccount} className="text-red-700 cursor-pointer">Delete Account</span>
-        <span className="text-red-700 cursor-pointer">Sign out</span>
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">Sign out</span>
       </div>
       <p className="text-red-700 mt-5">{ error && "Something went wrong!"}</p>
       <p className="text-green-700 mt-5">{ updateSuccess && "User is updated successfully!"}</p>
